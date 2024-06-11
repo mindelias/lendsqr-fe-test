e66c50455390ff4220d004894a38130d3210a127
Aminat Shotade <aminatshotade@AVI-MAC-LT-008s-MacBook-Pro.local>
2024-06-11 09:55:26 +0100

    completed header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/images/logo.svg";
+import "./styles.scss";
+import { Col, Divider, Dropdown, Input, Row, Space } from "antd";
+import Bell from "../../assets/icons/bell.svg";
+import UserImg from "../../assets/images/user-img.svg";
+import DropdownIcon from "../../assets/icons/dropdown.svg"
+const { Search } = Input;
 
 const Header = () => {
+  const items = [
+    {
+      key: "1",
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+          1st menu item
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+          2nd menu item
+        </a>
+      ),
+    },
+  ];
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="header-container">
+      <Row className="item-left" align="middle" justify={"space-between"}>
+        <Col span={4} className="logo-cont">
+          <img src={Logo} className="logo" alt="logo" />
+        </Col>
 
-export default Header
+        <div className="search-wrapper">
+          <Search
+            color="#39CDCC"
+            placeholder="input search text"
+            onSearch={(value) => console.log(value)}
+            size="large"
+            enterButton
+          />
+        </div>
+      </Row>
+      <Row className="item-right" align="middle" justify="end" gutter={20}>
+        <Col>
+          <a href="http://"> Docs</a>
+        </Col>
+        <Col>
+          <img src={Bell} alt="" />
+        </Col>
+        <Col>
+          <img className="user-image" src={UserImg} alt="" />
+        </Col>
+        <Col>
+          <Dropdown menu={{ items }}>
+            <a onClick={(e) => e.preventDefault()} href="/">
+              <Space>
+                Adedeji
+                <img src={DropdownIcon} alt="" />
+              </Space>
+            </a>
+          </Dropdown>
+        </Col>
+      </Row>
+    </nav>
+  );
+};
+
+export default Header;
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -4,44 +4,44 @@ import MenuItem from "../MenuItem/MenuItem";
 import "./styles.scss";
 import { useLocation, useNavigate } from "react-router-dom";
 
-
 const SideBar = () => {
-  const [activePath, setActivePath] = useState('/users');
+  const [activePath, setActivePath] = useState("/users");
   const navigate = useNavigate();
   const location = useLocation();
 
   return (
     <aside className="sidebar-container">
-      {menu.map((menu) => {
-        if (menu?.hasSubMenu) {
-          return (
-            <div  key={menu.key}>
-              <p className="submenu-title">{menu.title}</p>
-              {menu.subMenu?.map((subMenu) => (
-                <MenuItem
-                  key={subMenu.key}
-                  item={subMenu}
-                  // onClick={() => handleClick(subMenu, subMenu.path)}
-                  active={location.pathname === subMenu.path}
-                  activePath={activePath}
-                  setActivePath={setActivePath}
-                />
-              ))}
-            </div>
-          );
-        } else {
-          return (
-            <MenuItem
-               key={menu.key}
-               item={menu}
-               active={location.pathname === menu.path}
-               setActivePath={setActivePath}
-               activePath={activePath}
-               
-            />
-          );
-        }
-      })}
+      <div className="sidebar-content">
+        {menu.map((menu) => {
+          if (menu?.hasSubMenu) {
+            return (
+              <div key={menu.key}>
+                <p className="submenu-title">{menu.title}</p>
+                {menu.subMenu?.map((subMenu) => (
+                  <MenuItem
+                    key={subMenu.key}
+                    item={subMenu}
+                    // onClick={() => handleClick(subMenu, subMenu.path)}
+                    active={location.pathname === subMenu.path}
+                    activePath={activePath}
+                    setActivePath={setActivePath}
+                  />
+                ))}
+              </div>
+            );
+          } else {
+            return (
+              <MenuItem
+                key={menu.key}
+                item={menu}
+                active={location.pathname === menu.path}
+                setActivePath={setActivePath}
+                activePath={activePath}
+              />
+            );
+          }
+        })}
+      </div>
     </aside>
   );
 };
