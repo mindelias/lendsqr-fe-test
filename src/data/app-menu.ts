de17b7cafc468437d3348d094e7e6c97bce6bb17
Aminat Shotade <aminatshotade@AVI-MAC-LT-008s-MacBook-Pro.local>
2024-06-14 21:18:39 +0100

    created responsive navbar

--- src/data/app-menu.ts
+++ src/data/app-menu.ts
@@ -1,51 +1,190 @@
-import Briefcase from '../assets/icons/briefcase-active.svg';
-import Users from '../assets/icons/users.svg';
-import UserTimes from '../assets/icons/user-times.svg';
-import UserFriends from '../assets/icons/user-friends.svg';
-import Home from '../assets/icons/home.svg';
+import Briefcase from "../assets/icons/briefcase-active.svg";
+import Users from "../assets/icons/activate-user.svg";
+import UserTimes from "../assets/icons/user-times.svg";
+import UserFriends from "../assets/icons/user-friends.svg";
+import Home from "../assets/icons/home.svg";
+import Gurantor from "../assets/icons/guarantor.svg";
 export const menu = [
   {
     key: 1,
-    title: 'Switch Organization',
-    name: 'switch-organization',
-    path: '/dashboard',
-    icon:Briefcase
+    title: "Switch Organization",
+    name: "switch-organization",
+    path: "/dashboard",
+    icon: Briefcase,
   },
   {
     key: 2,
-    title: 'Dashboard',
-    name: 'dashboard',
-    path: '/dashboard',
-    icon: Home
+    title: "Dashboard",
+    name: "dashboard",
+    path: "/dashboard",
+    icon: Home,
   },
   {
     key: 3,
-    title: 'Customers',
-    name: 'customers',
+    title: "Customers",
+    name: "customers",
     hasSubMenu: true,
     subMenu: [
       {
         key: 4,
-        title: 'Users',
-        name: '/users',
-        path: '/users',
+        title: "Users",
+        name: "/users",
+        path: "/users",
         icon: UserFriends,
       },
       {
-        key: 5,
-        title: 'Karma',
-        name: 'karma',
-        path: '/karma',
-        icon: UserTimes
+        key: 6,
+        title: "Guarantors",
+        name: "guarantors",
+        path: "/guarantors",
+        icon: Gurantor,
       },
       {
-        key: 6,
-        title: 'Guarantors',
-        name: 'guarantors',
-        path: '/guarantors',
-        icon: UserFriends
+        key: 7,
+        title: "Loans",
+        name: "loans",
+        path: "/loans",
+        icon: UserTimes,
       },
-       
+      {
+        key: 8,
+        title: "Decision Models",
+        name: "decision-models",
+        path: "/decision-models",
+        icon: UserTimes,
+      },
+      {
+        key: 9,
+        title: "Savings",
+        name: "savings",
+        path: "/savings",
+        icon: UserTimes,
+      },
+      {
+        key: 10,
+        title: "Loan Requests",
+        name: "loan-requests",
+        path: "/loan-requests",
+        icon: UserTimes,
+      },
+      {
+        key: 11,
+        title: "Whitelist",
+        name: "whitelist",
+        path: "/whitelist",
+        icon: UserTimes,
+      },
+      {
+        key: 12,
+        title: "karma",
+        name: "karma",
+        path: "/karma",
+        icon: UserTimes,
+      },
+    ],
+  },
+
+  {
+    key: 13,
+    title: "Businesses",
+    name: "businesses",
+    hasSubMenu: true,
+    subMenu: [
+       {
+          key: 14,
+          title: "Organization",
+          name: "organization",
+          path: "/organization",
+          icon: Briefcase,
+       },
+       {
+          key: 15,
+          title: "Loan Products",
+          name: "loan-products",
+          path: "/loan-products",
+          icon: Briefcase,
+       },
+       {
+          key: 16,
+          title: "Savings Products",
+          name: "savings-products",
+          path: "/savings-products",
+          icon: Briefcase,
+       },
+       {
+          key: 17,
+          title: "Fees and Charges",
+          name: "fees-and-charges",
+          path: "/fees-and-charges",
+          icon: Briefcase,  
+       },
+
+       {
+          key: 18,
+          title: "Transactions",
+          name: "transactions",
+          path: "/transactions",
+          icon: Briefcase,
+       },
+       {
+          key: 19,
+          title: "Services",
+          name: "services",
+          path: "/services",
+          icon: Briefcase,
+       },
+       {
+          key: 20,
+          title: "Service Account",
+          name: "service-account",
+          path: "/service-account",
+          icon: Briefcase,
+       },
+
+       {
+          key: 21,
+          title: "Settlements",
+          name: "settlements",
+          path: "/settlements",
+          icon: Briefcase,
+       },
+
+       {
+          key: 22,
+          title: "Reports",
+          name: "reports",
+          path: "/reports",
+          icon: Briefcase,
+       },
+    ],
+  },
+  {
+    key: 23,
+    title: "Settings",
+    name: "settings",
+    hasSubMenu: true,
+    subMenu: [
+      {
+        key: 24,
+        title: "Preferences",
+        name: "preferences",
+        path: "/preferences",
+        icon: Briefcase,
+      },
+      {
+        key: 25,
+        title: "Fees and Pricing",
+        name: "fees-and-pricing",
+        path: "/fees-and-pricing",
+        icon: Briefcase,
+      },
+      {
+        key: 26,
+        title: "Audit Logs",
+        name: "audit-logs",
+        path: "/audit-logs",
+        icon: Briefcase,
+      },
     ],
   },
 ];
--- src/layouts/AppWrapper.tsx
+++ src/layouts/AppWrapper.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import Header from '../components/Header/Header';
-import Sidebar from '../components/SideBar/SideBar';
-import './styles.scss'
+import React, { useState } from "react";
+import Header from "../components/Header/Header";
+import Sidebar from "../components/SideBar/SideBar";
+import "./styles.scss";
+import useLayoutStore from "../store/layout";
 // import Navbar from '../components/Navbar';
 
-
-
 const AppWrapper = ({ children }: LayoutProps) => {
-  const [activePath, setActivePath] = useState('/users');
+  const [activePath, setActivePath] = useState("/users");
+  const { isSidebarCollapsed, toggleSidebar } = useLayoutStore();
 
   return (
     <div className="app-wrapper">
       <Header />
       <Sidebar />
-       
+
       <div className="main-content">
         {/* <Navbar /> */}
         {children}
