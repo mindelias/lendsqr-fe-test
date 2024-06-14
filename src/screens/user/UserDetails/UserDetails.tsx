a80715b4236116d6d1bad2aa0d28038bde845339
Aminat Shotade <aminatshotade@AVI-MAC-LT-008s-MacBook-Pro.local>
2024-06-14 13:07:11 +0100

    created list of tab components

--- src/screens/user/UserDetails/UserDetails.tsx
+++ src/screens/user/UserDetails/UserDetails.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Button, Col, Row, Space } from "antd";
-import Avatar from "../../../assets/icons/avatar.svg";
-import StarGoldFill from "../../../assets/icons/star-gold-filled.svg";
-import StarGold from "../../../assets/icons/star-gold.svg";
-import "./styles.scss";
+import TabList from "../../../components/Tabs/TabList";
+import UserSummaryCard from "../../../components/User/UserDetails/UserSummaryCard/UserSummaryCard";
+import UserProfile from "../../../components/User/UserDetails/UserProfile/UserProfile";
+
+import './styles.scss';
+
+ 
 
 const UserDetails = () => {
+  const [activeTab, setActiveTab] = React.useState("general-details");
   return (
     <div>
       <Row justify={"space-between"} align={"middle"} className="mb-3" gutter={[16, 16]}>
@@ -24,37 +28,18 @@ const UserDetails = () => {
         </Col>
       </Row>
       <div className="user-details-wrapper">
-        <div className="card-style">
-          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 16]} align={"middle"}> 
-            <Col>
-              <Row align={"middle"} gutter={[16, 16]}>
-                <Col>
-                  <div className="avatar">
-                    <img src={Avatar} alt="" />
-                  </div>
-                </Col>
-                <Col>
-                  <p className="app-heading--4 mb-10">Grace Effiom</p>
-                  <p className="app-paragraph text-left">LSQFf587g90</p>
-                </Col>
-              </Row>
-            </Col>
-            <Col>
-              <div className="user-tier">
-                <p className="app-paragraph mb-10">User's Tier</p>
-                <Space>
-                  <img src={StarGoldFill} alt="" />
-                  <img src={StarGold} alt="" />
-                  <img src={StarGold} alt="" />
-                </Space>
-              </div>
-            </Col>
-            <Col>
-              <p className="app-heading--4 mb-10 text-left">₦200,000.00</p>
+        <div className="card-style mb-lg pb-0">
+          <UserSummaryCard />
+          <TabList onTabClick={(tab) => setActiveTab(tab)} activeTab={activeTab} />
+        </div>
 
-              <p className="app-paragraph">9912345678/Providus Bank</p>
-            </Col>
-          </Row>
+        <div className="card-style h-min-300">
+          {activeTab === "general-details" &&  <UserProfile/>}
+          {activeTab === "documents" && <div>Documents</div>}
+          {activeTab === "bank-details" && <div>Bank Details</div>}
+          {activeTab === "loans" && <div>Loans</div>}
+          {activeTab === "savings" && <div>Savings</div>}
+          {activeTab === "app-and-system" && <div>App and System</div>}
         </div>
       </div>
     </div>
