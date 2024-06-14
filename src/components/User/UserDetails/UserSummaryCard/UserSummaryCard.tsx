4f45fb9e04fcdd41555af6474060cc420d6b8ef1
Aminat Shotade <aminatshotade@AVI-MAC-LT-008s-MacBook-Pro.local>
2024-06-14 12:47:39 +0100

    Created user summary card

---
+++ src/components/User/UserDetails/UserSummaryCard/UserSummaryCard.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Row, Col,  Space } from 'antd'
+import Avatar from '../../../../assets/icons/avatar.svg'
+import StarGoldFill from '../../../../assets/icons/star-gold-filled.svg'
+import StarGold from '../../../../assets/icons/star-gold.svg'
+
+const UserSummaryCard = () => {
+  return (
+    <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 16]} align={"middle"}>
+    <Col>
+      <Row align={"middle"} gutter={[16, 16]}>
+        <Col>
+          <div className="avatar">
+            <img src={Avatar} alt="" />
+          </div>
+        </Col>
+        <Col>
+          <p className="app-heading--4 mb-10">Grace Effiom</p>
+          <p className="app-paragraph text-left">LSQFf587g90</p>
+        </Col>
+      </Row>
+    </Col>
+    <Col>
+      <div className="user-tier">
+        <p className="app-paragraph mb-10">User's Tier</p>
+        <Space>
+          <img src={StarGoldFill} alt="" />
+          <img src={StarGold} alt="" />
+          <img src={StarGold} alt="" />
+        </Space>
+      </div>
+    </Col>
+    <Col>
+      <p className="app-heading--4 mb-10 text-left">₦200,000.00</p>
+
+      <p className="app-paragraph">9912345678/Providus Bank</p>
+    </Col>
+  </Row>
+  )
+}
+
+export default UserSummaryCard
