04698db8bb80c824aab771e0ee5ec914696444d1
Aminat Shotade <aminatshotade@AVI-MAC-LT-008s-MacBook-Pro.local>
2024-06-17 21:31:43 +0100

    specify route names and definite path

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -20,9 +20,108 @@ const routes = [
   },
   {
     id: 4,
-    path: "/coming-soon",
+    path: "/dashboard",
     element: <ComingSoon />,
   },
+  {
+    id: 5,
+  path: "/guarantors",
+  element: <ComingSoon />,
+  },
+  {
+    id: 6,
+    path: "/loans",
+    element: <ComingSoon />,
+  },
+  {
+    id: 7,
+    path: "/decision-models",
+    element: <ComingSoon />,
+  },
+  {
+    id: 8,
+    path: "/savings",
+    element: <ComingSoon />,
+  },
+  {
+    id: 9,
+    path: "/loan-requests",
+    element: <ComingSoon />,
+  },
+  {
+    id: 10,
+    path: "/whitelist",
+    element: <ComingSoon />,
+  },
+  {
+    id: 11,
+    path: "/karma",
+    element: <ComingSoon />,
+  },
+  {
+    id: 12,
+    path: "/organization",
+    element: <ComingSoon />,
+  },
+  {
+    id: 13,
+    path: "/loan-products",
+    element: <ComingSoon />,
+  },
+  {
+    id: 14,
+    path: "/savings-products",
+    element: <ComingSoon />,
+  },
+  {
+    id: 15,
+    path: "/fees-and-charges",
+    element: <ComingSoon />,
+  },
+  {
+    id: 16,
+    path: '/transactions',
+  },
+  {
+    id: 17,
+    path: "/services",
+    element: <ComingSoon />,
+  },
+  {
+    id: 18,
+    path: "/service-account",
+    element: <ComingSoon />,
+  },
+  {
+    id: 19,
+    path: "/settlements",
+    element: <ComingSoon />,
+  },
+  {
+    id: 20,
+    path: "/reports",
+    element: <ComingSoon />,
+  },
+  {
+    id: 21,
+    path: "/preferences",
+    element: <ComingSoon />,
+  },
+  {
+    id: 22,
+    path: "/fees-and-pricing",
+    element: <ComingSoon />,
+  },
+  {
+
+    id: 23,
+    path: "/audit-logs",
+    element: <ComingSoon />,
+  },
+  
+
+   
+
 ];
 
 export default routes;
